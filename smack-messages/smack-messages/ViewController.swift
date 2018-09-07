b076acd54231feb734a3affd25d7cad2da67643b
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-07 07:59:16 -0400

    first commit

---
+++ smack-messages/smack-messages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack-messages
+//
+//  Created by Steven McGrath on 9/7/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
